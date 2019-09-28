46b5d9b5a9ee910ec3e8c04d67bc99b0cc611fcd
makera <makars999mail@gmail.com>
2019-09-28 22:15:45 +0900

    admin

--- data_stat/admin.py
+++ data_stat/admin.py
@@ -1,11 +1,26 @@
 from django.contrib import admin
 from .models import VoteQuestion, VoteAnswer, Quiz, QuizQuestion, QuizAnswer, VoteUsersAnswer, QuizUsersAnswer
 
-
-admin.site.register(VoteQuestion)
-admin.site.register(VoteAnswer)
 admin.site.register(Quiz)
-admin.site.register(QuizQuestion)
-admin.site.register(QuizAnswer)
 admin.site.register(VoteUsersAnswer)
 admin.site.register(QuizUsersAnswer)
+
+
+class VoteAnswerTabularInline(admin.TabularInline):
+    model = VoteAnswer
+
+
+@admin.register(VoteQuestion)
+class VoteQuestionAdmin(admin.ModelAdmin):
+    list_display = ('text',)
+    list_display_links = ('text',)
+    inlines = [VoteAnswerTabularInline, ]
+
+
+class QuizAnswerTabularInline(admin.TabularInline):
+    model = QuizAnswer
+
+
+@admin.register(QuizQuestion)
+class QuizQuestionAdmin(admin.ModelAdmin):
+    inlines = [QuizAnswerTabularInline, ]
