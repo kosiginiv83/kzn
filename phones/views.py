f8c91aeb37e41e5e86ff25e3c6337f15d5a971e1
makera <makars999mail@gmail.com>
2019-09-29 09:43:46 +0900

    close send sms

--- phones/views.py
+++ phones/views.py
@@ -23,8 +23,8 @@ class CodeSerializer(serializers.ModelSerializer):
 def randomString(stringLength=5):
     """Generate a random string of fixed length """
     letters = string.digits
-    return ''.join(random.choice(letters) for i in range(stringLength))
-
+    # return ''.join(random.choice(letters) for i in range(stringLength))
+    return '00000';
 
 class PhoneViewSet(viewsets.ViewSet):
     @decorators.action(methods=['post'], detail=False, permission_classes=[permissions.AllowAny])
@@ -34,9 +34,9 @@ class PhoneViewSet(viewsets.ViewSet):
             phone = Phone.objects.get_or_create(phone=serializer.data['phone'])[0]
             code = Code.objects.create(phone=phone, code=randomString(),
                                        end=datetime.datetime.now() + datetime.timedelta(minutes=20))
-            url = 'https://sms.ru/sms/send?api_id=903cf685-12bd-b4e4-31ef-a57dd97efe4b&json=1&to=%s&msg=Your+code+%s' % (
-                phone.phone, code.code)
-            urlopen(url)
+            # url = 'https://sms.ru/sms/send?api_id=903cf685-12bd-b4e4-31ef-a57dd97efe4b&json=1&to=%s&msg=Your+code+%s' % (
+            #     phone.phone, code.code)
+            # urlopen(url)
             return response.Response({'status': 'ok'})
         return response.Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
